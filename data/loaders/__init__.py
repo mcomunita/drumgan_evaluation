f78f1886ba0a25a5e044fb0e94207dfe3992e66e
mcomunita <m.comunita@qmul.ac.uk>
2021-04-28 20:22:11 +0200

    setup to train inception on footsteps

---
+++ data/loaders/__init__.py
@@ -0,0 +1,26 @@
+# from .nsynth import NSynth
+# from .mtg_drums import MTGDrums
+# from .csl_drums import CSLDrums
+# from .youtube_pianos import YouTubePianos
+# from .sinewaves import Sinewaves
+# from .mp3towav_loader import MP3ToWAV
+from .footsteps_loader import FootstepsDataLoader
+from .base_loader import SimpleLoader
+
+AVAILABLE_DATASETS = {
+	# 'nsynth': NSynth,
+	# 'mtg-drums': MTGDrums,
+	# 'csl-drums': CSLDrums,
+	# 'youtube-pianos': YouTubePianos,
+	# 'sinewaves': Sinewaves,
+	# 'mp3towav': MP3ToWAV,
+	'footsteps': FootstepsDataLoader,
+	'default': SimpleLoader
+}
+
+def get_data_loader(name):
+    if name not in AVAILABLE_DATASETS:
+    	raise AttributeError(f"Invalid module name. \
+                               Available: {AVAILABLE_DATASETS.keys()}")
+
+    return AVAILABLE_DATASETS[name]
---
+++ data/loaders/footsteps_loader.py
@@ -0,0 +1,15 @@
+from .base_loader import AudioDataLoader
+from ..db_extractors.footsteps_extractor import extract
+
+
+class FootstepsDataLoader(AudioDataLoader):
+    def load_data(self):
+        print('-- FOOSTEPS DATALOADER: load_data')
+        print()
+        self.data, self.metadata = \
+            extract(self.data_path, self.criteria)
+        print('len(self.data):')
+        print(len(self.data))
+        print('len(self.metadata): ')
+        print(len(self.metadata))
+        print()
